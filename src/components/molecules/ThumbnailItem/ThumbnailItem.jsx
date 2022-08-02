8c1686f520ede20ccd6d3023df17fda093b0d5aa
immprada <im.mprada@gmail.com>
2022-08-02 14:04:29 -0500

    Create component ThumbnailItem mockup

--- src/components/molecules/ThumbnailItem/ThumbnailItem.jsx
+++ src/components/molecules/ThumbnailItem/ThumbnailItem.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   ThumbnailCard,
   LoadingImageIndicator
 } from '../../atoms';
+import './styles.scss';
 
-const ThumbnailItem = () => {
+
+const renderAtom = (isLoading, artwork) => {
+  if(isLoading) return <LoadingImageIndicator />
 
   return (
-    <h1>HELLO WORLD</h1>
+    <ThumbnailCard
+      artwork={artwork}
+    />
+  )
+}
+
+const ThumbnailItem = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [artwork, setArtWork] = useState(null);
+
+  const newArtwork = () => (
+    setArtWork({
+      isLoading: false,
+      sourceUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Johannes_Vermeer_%281632-1675%29_-_The_Girl_With_The_Pearl_Earring_%281665%29.jpg/1200px-Johannes_Vermeer_%281632-1675%29_-_The_Girl_With_The_Pearl_Earring_%281665%29.jpg',
+      title: 'Girl with a Pearl Earring',
+      author: 'Johannes Vermeer',
+    })
+  )
+
+  useEffect(() => {setTimeout(newArtwork, 2000)}, [])
+  
+  useEffect(() => {
+    if(artwork) setIsLoading(false)
+  }, [artwork])
+
+
+  return (
+    <div className="thumbnail-item">
+      {renderAtom(isLoading, artwork)}
+    </div>
   )
 }
 
