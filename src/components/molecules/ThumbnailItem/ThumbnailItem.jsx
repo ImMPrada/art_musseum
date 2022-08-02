2ef2ad7fe61b4ace51ee9dab3f94af8796033493
immprada <im.mprada@gmail.com>
2022-08-02 13:50:54 -0500

    Add ThumbnailItem mockup

---
+++ src/components/molecules/ThumbnailItem/ThumbnailItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  ThumbnailCard,
+  LoadingImageIndicator
+} from '../../atoms';
+
+const ThumbnailItem = () => {
+
+  return (
+    <h1>HELLO WORLD</h1>
+  )
+}
+
+export default ThumbnailItem;
